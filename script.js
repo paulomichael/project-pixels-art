5caa137f33a6ab238f34fff98ad1b83750ec6b72
paulomichael <paulo.michael@gmail.com>
2021-05-06 02:21:36 -0300

    requisito 4 - quadro de pixels + muda os estilos do JS pro CSS

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// 1 - Adicione à página o título "Paleta de Cores".
-// O título deverá ficar dentro de uma tag h1 com o id denominado title;
-// O texto do título deve ser exatamente "Paleta de Cores".
-// O que será verificado:
-// Verifica se contém um elemento h1 com o id title com o título correto
+/* 1 - Adicione à página o título "Paleta de Cores".
+O título deverá ficar dentro de uma tag h1 com o id denominado title;
+O texto do título deve ser exatamente "Paleta de Cores".
+O que será verificado:
+Verifica se contém um elemento h1 com o id title com o título correto */
 
 let h1 = document.createElement('h1')
 h1.id = 'title'
 h1.innerHTML = 'Paleta de Cores'
 document.body.appendChild(h1)
 
-// 2 - Adicione à página uma paleta de quatro cores distintas.
-// A paleta de cores deve ser um elemento com id denominado color-palette, ao passo que cada cor individual da paleta de cores deve possuir a classe color;
-// A cor de fundo de cada elemento da paleta deverá ser a cor que o elemento representa. A única cor não permitida na paleta é a cor branca.;
-// Cada elemento da paleta de cores deverá ter uma borda preta, sólida e com 1 pixel de largura;
-// A paleta de cores deverá listar todas as cores disponíveis para utilização lado a lado, e deverá ser posicionada abaixo do título "Paleta de Cores";
-// A paleta de cores não deve conter cores repetidas.
-// O que será verificado:
-// A paleta de cores deve ser um elemento com id denominado color-palette
-// Verifica se cada cor individual da paleta de cores possui a classe chamada color.
-// Verifica se a cor de fundo de cada elemento da paleta é a cor que o elemento representa. A única cor não permitida na paleta é a cor branca.
-// Verifica se cada elemento da paleta de cores tem uma borda preta, sólida e com 1 pixel de largura;
-// Verifica se a paleta lista todas as cores disponíveis para utilização, lado a lado.
-// Verifica se a paleta de cores está posicionada abaixo do título 'Paleta de Cores'
-// Verifica se a paleta de cores não contém cores repetidas.
+/* 2 - Adicione à página uma paleta de quatro cores distintas.
+A paleta de cores deve ser um elemento com id denominado color-palette, ao passo que cada cor individual da paleta de cores deve possuir a classe color;
+A cor de fundo de cada elemento da paleta deverá ser a cor que o elemento representa. A única cor não permitida na paleta é a cor branca.;
+Cada elemento da paleta de cores deverá ter uma borda preta, sólida e com 1 pixel de largura;
+A paleta de cores deverá listar todas as cores disponíveis para utilização lado a lado, e deverá ser posicionada abaixo do título "Paleta de Cores";
+A paleta de cores não deve conter cores repetidas.
+O que será verificado:
+A paleta de cores deve ser um elemento com id denominado color-palette
+Verifica se cada cor individual da paleta de cores possui a classe chamada color.
+Verifica se a cor de fundo de cada elemento da paleta é a cor que o elemento representa. A única cor não permitida na paleta é a cor branca.
+Verifica se cada elemento da paleta de cores tem uma borda preta, sólida e com 1 pixel de largura;
+Verifica se a paleta lista todas as cores disponíveis para utilização, lado a lado.
+Verifica se a paleta de cores está posicionada abaixo do título 'Paleta de Cores'
+Verifica se a paleta de cores não contém cores repetidas. */
 
-// 3 - Adicione a cor preta como a primeira cor da paleta de cores.
-// O que será verificado:
-// Verifica se a primeira cor da paleta é preta
-// Verifica se as demais cores podem ser escolhidas livremente.
+/* 3 - Adicione a cor preta como a primeira cor da paleta de cores.
+O que será verificado:
+Verifica se a primeira cor da paleta é preta
+Verifica se as demais cores podem ser escolhidas livremente. */
 
 let colorPalette = document.createElement('div')
 document.body.appendChild(colorPalette)
@@ -37,12 +37,63 @@ for (color of colorList) {
     let colorItem = document.createElement('div')
     colorItem.className = 'color'
     colorItem.style.backgroundColor = color
-    colorItem.style.borderColor = 'black'
-    colorItem.style.borderStyle = 'solid'
-    colorItem.style.borderWidth = '1px'
-    colorItem.style.display = 'inline-block'
-    colorItem.style.width = '5px'
-    colorItem.style.height = '5px'
     colorPalette.appendChild(colorItem)
 }
 
+/* 4 - Adicione à página um quadro de pixels, com 25 pixels.
+O quadro de "pixels" deve ter 5 elementos de largura e 5 elementos de comprimento;
+
+O quadro de "pixels" deve possuir o id denominado pixel-board, ao passo que cada "pixel" individual dentro do quadro deve possuir a classe denominada pixel;
+
+A cor inicial dos "pixels" dentro do quadro, ao abrir a página, deve ser branca;
+
+O quadro de "pixels" deve aparecer abaixo da paleta de cores.
+
+O que será verificado:
+
+Verifica se o quadro de pixels possui o id denominado pixel-board
+
+Verifica se cada pixel individual dentro do quadro possui a classe denominada pixel.
+
+Verifica se a cor inicial dos pixels dentro do quadro, ao abrir a página, é branca.
+
+Verifica se o quadro de pixels aparece abaixo da paleta de cores */
+/* Creditos a https://stackoverflow.com/questions/29229523/how-and-why-to-use-display-table-cell-css */
+/* HTML
+<div id="table">
+    <div class="tr">
+        <div class="td"></div>
+        <div class="td"></div>
+        <div class="td"></div>
+    </div>
+    <div class="tr">
+        <div class="td"></div>
+        <div class="td"></div>
+        <div class="td"></div>
+    </div>
+</div>
+CSS
+#table{ 
+    display: table; 
+}
+.tr{ 
+    display: table-row; 
+}
+.td{ 
+    display: table-cell; } */
+let pixelBoard = document.createElement('div')
+document.body.appendChild(pixelBoard)
+pixelBoard.id = 'pixel-board'
+let pixelLines = 5;
+let pixelColumns = 5;
+for (let lineIndex = 0; lineIndex < pixelLines; lineIndex += 1) {
+    let line = document.createElement('div')
+    pixelBoard.appendChild(line)
+    line.className = 'line'
+    for (let columnIndex = 0; columnIndex < pixelColumns; columnIndex += 1) {
+        let pixel = document.createElement('div')
+        line.appendChild(pixel)
+        pixel.className = 'pixel'
+    }
+}
+// window.onload
